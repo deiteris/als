2ba6cc134919046c12caa6f67eccb442a10797f0
agusayerza <agusayerza@gmail.com>
2021-07-28 18:36:39 -0300

    Clean

--- als-server/shared/src/main/scala/org/mulesoft/als/server/modules/diagnostic/CustomProfileDiagnosticManager.scala
+++ als-server/shared/src/main/scala/org/mulesoft/als/server/modules/diagnostic/CustomProfileDiagnosticManager.scala
@@ -9,17 +9,15 @@ import amf.internal.environment.Environment
 import amf.{ProfileName, ProfileNames}
 import org.mulesoft.als.server.client.ClientNotifier
 import org.mulesoft.als.server.logger.Logger
-import org.mulesoft.als.server.modules.ast.ResolvedUnitListener
-import org.mulesoft.als.server.modules.common.reconciler.Runnable
+import org.mulesoft.als.server.modules.ast.AstListener
 import org.mulesoft.als.server.modules.customvalidation.RegisterProfileManager
 import org.mulesoft.als.server.workspace.WorkspaceManager
 import org.mulesoft.amfintegration.AmfImplicits.BaseUnitImp
-import org.mulesoft.amfintegration.{AmfInstance, AmfResolvedUnit, DiagnosticsBundle}
-import org.mulesoft.lsp.feature.telemetry.{MessageTypes, TelemetryProvider}
+import org.mulesoft.amfintegration.AmfResolvedUnit
+import org.mulesoft.lsp.feature.telemetry.TelemetryProvider
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{Future, Promise}
-import scala.util.{Failure, Success}
+import scala.concurrent.Future
 
 class CustomProfileDiagnosticManager(override protected val telemetryProvider: TelemetryProvider,
                                      override protected val clientNotifier: ClientNotifier,
@@ -31,38 +29,23 @@ class CustomProfileDiagnosticManager(override protected val telemetryProvider: T
                                      val validatorBuilder: ValidatorBuilder,
                                      val platform: Platform)
     extends DiagnosticManager
-    with ResolvedUnitListener {
+    with AstListener[AmfResolvedUnit] {
 
   override protected val managerName: DiagnosticManagerKind = CustomValidationDiagnosticKind
-  override type RunType = CustomValidationRunnable
 
-  val instance                                   = AmfInstance(env)
   var workspaceManager: Option[WorkspaceManager] = None
 
   def withWorkspaceManager(workspaceManager: WorkspaceManager): Unit = this.workspaceManager = Some(workspaceManager)
 
-  protected override def runnable(ast: AmfResolvedUnit, uuid: String): RunType =
-    new CustomValidationRunnable(ast.originalUnit.identifier, ast, uuid)
-
-  protected override def onFailure(uuid: String, uri: String, exception: Throwable): Unit = {
-    logger.error(s"Error on validation: ${exception.toString}", "CustomValidationDiagnosticManager", "newASTAvailable")
-    exception.printStackTrace()
-    clientNotifier.notifyDiagnostic(ValidationReport(uri, Set.empty, ProfileNames.AMF).publishDiagnosticsParams)
-  }
-
-  protected override def onSuccess(uuid: String, uri: String): Unit =
-    logger.debug(s"End report: $uuid", "CustomValidationRunnable", "newASTAvailable")
-
   /**
-    * Meant just for logging
+    * Called on new AST available
     *
-    * @param resolved
-    * @param uuid
+    * @param ast  - AST
+    * @param uuid - telemetry UUID
     */
-  override protected def onNewAstPreprocess(resolved: AmfResolvedUnit, uuid: String): Unit =
-    logger.debug("Running custom validations on:\n" + resolved.originalUnit.id,
-                 "CustomValidationDiagnosticManager",
-                 "newASTAvailable")
+  override def onNewAst(ast: AmfResolvedUnit, uuid: String): Unit = {
+    gatherValidationErrors(ast.originalUnit.identifier, ast, uuid)
+  }
 
   override def onRemoveFile(uri: String): Unit = {
     validationGatherer.removeFile(uri, managerName)
@@ -74,10 +57,7 @@ class CustomProfileDiagnosticManager(override protected val telemetryProvider: T
       .map(bu => bu.location().getOrElse(bu.id))
       .toSet + baseUnit.location().getOrElse(baseUnit.id)
 
-  private def gatherValidationErrors(uri: String,
-                                     resolved: AmfResolvedUnit,
-                                     references: Map[String, DiagnosticsBundle],
-                                     uuid: String): Future[Unit] = {
+  private def gatherValidationErrors(uri: String, resolved: AmfResolvedUnit, uuid: String): Future[Unit] = {
     val startTime = System.currentTimeMillis()
 
     for {
@@ -95,7 +75,11 @@ class CustomProfileDiagnosticManager(override protected val telemetryProvider: T
       results.foreach(println)
       validationGatherer
         .indexNewReport(ErrorsWithTree(uri, results.toSeq, Some(tree(resolved.originalUnit))), managerName, uuid)
-      notifyReport(uri, resolved.originalUnit, references, managerName, ProfileName("CustomValidation"))
+      notifyReport(uri,
+                   resolved.originalUnit,
+                   resolved.diagnosticsBundle,
+                   managerName,
+                   ProfileName("CustomValidation"))
 
       val endTime = System.currentTimeMillis()
       this.logger.debug(s"It took ${endTime - startTime} milliseconds to validate with Go env",
@@ -114,7 +98,6 @@ class CustomProfileDiagnosticManager(override protected val telemetryProvider: T
     println("_________")
     val report = new OPAValidatorReportLoader().load(r)
     println(report)
-    println("result first: " + report.results.head.toString())
     val copied = report.copy(results = report.results.map { r =>
       val element = unit
         .findById(r.targetNode)
@@ -123,8 +106,6 @@ class CustomProfileDiagnosticManager(override protected val telemetryProvider: T
         .map(_.location)
       r.copy(location = location)
     })
-
-    println("copied first: " + copied.results.head.toString())
     copied
   }
 
@@ -140,44 +121,4 @@ class CustomProfileDiagnosticManager(override protected val telemetryProvider: T
       case e: Exception            => Future.failed(e)
     }
   }
-
-  class CustomValidationRunnable(var uri: String, ast: AmfResolvedUnit, uuid: String) extends Runnable[Unit] {
-    private var canceled = false
-
-    private val kind = "CustomValidationRunnable"
-
-    def run(): Promise[Unit] = {
-      val promise = Promise[Unit]()
-
-      def innerRunGather() =
-        gatherValidationErrors(ast.originalUnit.identifier, ast, ast.diagnosticsBundle, uuid) andThen {
-          case Success(report) => promise.success(report)
-
-          case Failure(error) => promise.failure(error)
-        }
-
-      telemetryProvider.timeProcess(
-        "End report",
-        MessageTypes.BEGIN_CUSTOM_DIAGNOSTIC,
-        MessageTypes.END_CUSTOM_DIAGNOSTIC,
-        "CustomValidationRunnable : onNewAst",
-        uri,
-        innerRunGather,
-        uuid
-      )
-      promise
-    }
-
-    def conflicts(other: Runnable[Any]): Boolean =
-      other.asInstanceOf[CustomValidationRunnable].kind == kind && uri == other
-        .asInstanceOf[CustomValidationRunnable]
-        .uri
-
-    def cancel() {
-      canceled = true
-    }
-
-    def isCanceled(): Boolean = canceled
-  }
-
 }
